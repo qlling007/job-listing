7b6571e58bd9f3b8ab28a1a0412cd768df58f361
qinglingling <qlingling007@gmail.com>
2017-01-13 22:20:19 +0800

    implement part4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-    email == 'xdite@growth.school'
+    email == '2503107846@qq.com'
   end
   def admin?
     is_admin
